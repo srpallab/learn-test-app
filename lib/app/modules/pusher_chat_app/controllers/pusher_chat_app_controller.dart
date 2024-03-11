bbbdcd1d479bf685faafe64f2837357bae6148d9
Shafiqur Rahman <pallab.alif@gmail.com>
2024-03-11 17:27:09 +0600

    TODO: Pusher Chat.

--- lib/app/modules/pusher_chat_app/controllers/pusher_chat_app_controller.dart
+++ lib/app/modules/pusher_chat_app/controllers/pusher_chat_app_controller.dart
@@ -1,12 +1,9 @@
-import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pretty_http_logger/pretty_http_logger.dart';
+import 'package:laravel_echo/laravel_echo.dart';
 import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pusher_client_fixed/pusher_client_fixed.dart';
 
 class PusherChatAppController extends GetxController {
   PusherChannelsFlutter pusher = PusherChannelsFlutter.getInstance();
@@ -16,157 +13,156 @@ class PusherChatAppController extends GetxController {
   /// key = "69ef518953032858d64d"
   /// secret = "b0f4e94b427b3b7529ce"
   /// cluster = "ap1"
+  final String appId = "1475592";
+  final String secret = "b0f4e94b427b3b7529ce";
   final String apiKey = '69ef518953032858d64d';
   final String cluster = 'ap1';
+  final String channelName =
+      'presence-Chatroom.c1136e5f-450f-4229-8c17-72de6d7bf2ff';
+  final String token = "98193|ewclPpS3JKja0L2loygJ607GHS3SYZlskTJdoSKY";
+  final String authUrl = "https://icircles.app/api/broadcasting/auth";
 
-  /// Form Key
-  final channelFormKey = GlobalKey<FormState>();
-  final eventFormKey = GlobalKey<FormState>();
-
-  /// Text Inputs
-  final channelName = TextEditingController();
-  final eventName = TextEditingController();
-  final data = TextEditingController();
-
-  /// Scroll Controller
-  final ScrollController listViewController = ScrollController();
-
-  Future<void> initPlatformState() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    channelName.text = prefs.getString("channelName") ?? 'notifyChannel';
-    eventName.text = prefs.getString("eventName") ?? 'client-event';
-    data.text = prefs.getString("data") ?? 'test';
-  }
-
-  void onConnectPressed() async {
-    if (!channelFormKey.currentState!.validate()) {
-      return;
-    }
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString("apiKey", apiKey);
-    prefs.setString("cluster", cluster);
-    prefs.setString("channelName", channelName.text);
-
-    try {
-      await pusher.init(
-        apiKey: apiKey,
-        cluster: cluster,
-        useTLS: true,
-        onConnectionStateChange: onConnectionStateChange,
-        onError: onError,
-        onSubscriptionSucceeded: onSubscriptionSucceeded,
-        onEvent: onEvent,
-        onSubscriptionError: onSubscriptionError,
-        onDecryptionFailure: onDecryptionFailure,
-        onMemberAdded: onMemberAdded,
-        onMemberRemoved: onMemberRemoved,
-        // authEndpoint: "https://icircles.app/broadcasting/auth",
-        onAuthorizer: onAuthorizer,
-      );
-
-      await pusher.subscribe(channelName: channelName.text);
-      await pusher.connect();
-    } catch (e) {
-      log("ERROR: $e");
-    }
-  }
-
-  void onConnectionStateChange(dynamic currentState, dynamic previousState) {
-    log("Connection: $currentState");
-    isPusherConnected.value = true;
-  }
-
-  void onError(String message, int? code, dynamic e) {
-    log("onError: $message code: $code exception: $e");
-  }
-
-  void onEvent(PusherEvent event) {
-    log("onEvent: $event");
-  }
-
-  void onSubscriptionSucceeded(String channelName, dynamic data) {
-    log("onSubscriptionSucceeded: $channelName data: $data");
-    final me = pusher.getChannel(channelName)?.me;
-    log("Me: $me");
-  }
-
-  void onSubscriptionError(String message, dynamic e) {
-    log("onSubscriptionError: $message Exception: $e");
-  }
-
-  void onDecryptionFailure(String event, String reason) {
-    log("onDecryptionFailure: $event reason: $reason");
-  }
-
-  void onMemberAdded(String channelName, PusherMember member) {
-    log("onMemberAdded: $channelName user: $member");
-  }
-
-  void onMemberRemoved(String channelName, PusherMember member) {
-    log("onMemberRemoved: $channelName user: $member");
-  }
-
-  void onTriggerEventPressed() async {
-    var eventFormValidated = eventFormKey.currentState!.validate();
-
-    if (!eventFormValidated) {
-      return;
-    }
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString("eventName", eventName.text);
-    prefs.setString("data", data.text);
-
-    pusher.trigger(
-      PusherEvent(
-        channelName: channelName.text,
-        eventName: eventName.text,
-        data: data.text,
+  void initPusher() {
+    PusherOptions options = PusherOptions(
+      cluster: cluster,
+      auth: PusherAuth(
+        authUrl,
+        headers: {'Authorization': 'Bearer $token'},
       ),
     );
+    PusherClient pusherClient = PusherClient(
+      apiKey,
+      options,
+      autoConnect: false,
+      enableLogging: true,
+    );
+    Echo echo = Echo(
+      broadcaster: EchoBroadcasterType.Pusher,
+      client: pusherClient,
+    );
+    // Listening public channel
+    echo.channel(channelName).listen('PublicEvent', (e) {
+      log(e);
+    });
   }
 
   @override
   void onInit() {
     super.onInit();
-    initPlatformState();
+    initPusher();
   }
 
-  /// https://pub.dev/packages/crypto to get access to Hmac methods
-  // getSignature(String value) {
-  //   var key = utf8.encode('<your-pusher-app-secret>');
-  //   var bytes = utf8.encode(value);
+  // Future<void> initPlatformState() async {
+  //   try {
+  //     await pusher.init(
+  //       apiKey: apiKey,
+  //       cluster: cluster,
+  //       useTLS: true,
+  //       onConnectionStateChange: onConnectionStateChange,
+  //       onError: onError,
+  //       onSubscriptionSucceeded: onSubscriptionSucceeded,
+  //       onEvent: onEvent,
+  //       onSubscriptionError: onSubscriptionError,
+  //       onDecryptionFailure: onDecryptionFailure,
+  //       onMemberAdded: onMemberAdded,
+  //       onMemberRemoved: onMemberRemoved,
+  //       // authEndpoint: "https://icircles.app/broadcasting/auth",
+  //       onAuthorizer: onAuthorizer,
+  //     );
   //
-  //   var hmacSha256 = Hmac(sha256, key); // HMAC-SHA256
-  //   var digest = hmacSha256.convert(bytes);
-  //   print("HMAC signature in string is: $digest");
-  //   return digest;
+  //     await pusher.subscribe(channelName: channelName);
+  //     await pusher.connect();
+  //   } catch (e) {
+  //     log("ERROR: $e");
+  //   }
   // }
-
-  dynamic onAuthorizer(
-      String channelName, String socketId, dynamic options) async {
-    log("onAuthorizer $channelName : SocketId $socketId");
-    const String token = "60056|XQmYLu4O6AHiqNiqxwDOMEIuMSHmLCZVMSlIs7Y2";
-    var authUrl = "https://icircles.app/broadcasting/auth";
-
-    HttpWithMiddleware http = HttpWithMiddleware.build(middlewares: [
-      HttpLogger(logLevel: LogLevel.BODY),
-    ]);
-    dynamic result;
-    try {
-      result = await http.post(
-        Uri.parse(authUrl),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Bearer $token',
-        },
-        body: 'socket_id=$socketId&channel_name=$channelName',
-      );
-    } catch (e) {
-      log(e.toString());
-    }
-
-    var json = jsonDecode(result.body);
-    return json;
-  }
+  //
+  // void onConnectionStateChange(dynamic currentState, dynamic previousState) {
+  //   log("Connection: $currentState");
+  //   isPusherConnected.value = true;
+  // }
+  //
+  // void onError(String message, int? code, dynamic e) {
+  //   log("onError: $message code: $code exception: $e");
+  // }
+  //
+  // void onEvent(PusherEvent event) {
+  //   log("onEvent: $event");
+  // }
+  //
+  // void onSubscriptionSucceeded(String channelName, dynamic data) {
+  //   log("onSubscriptionSucceeded: $channelName data: $data");
+  //   final me = pusher.getChannel(channelName)?.me;
+  //   log("Me: $me");
+  // }
+  //
+  // void onSubscriptionError(String message, dynamic e) {
+  //   log("onSubscriptionError: $message Exception: $e");
+  // }
+  //
+  // void onDecryptionFailure(String event, String reason) {
+  //   log("onDecryptionFailure: $event reason: $reason");
+  // }
+  //
+  // void onMemberAdded(String channelName, PusherMember member) {
+  //   log("onMemberAdded: $channelName user: $member");
+  // }
+  //
+  // void onMemberRemoved(String channelName, PusherMember member) {
+  //   log("onMemberRemoved: $channelName user: $member");
+  // }
+  //
+  // void onTriggerEventPressed() async {
+  //   // pusher.trigger(
+  //   //   PusherEvent(
+  //   //     channelName: channelName.text,
+  //   //     eventName: eventName.text,
+  //   //     data: data.text,
+  //   //   ),
+  //   // );
+  // }
+  //
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  //   initPlatformState();
+  // }
+  //
+  // /// https://pub.dev/packages/crypto to get access to Hmac methods
+  // // getSignature(String value) {
+  // //   var key = utf8.encode('<your-pusher-app-secret>');
+  // //   var bytes = utf8.encode(value);
+  // //
+  // //   var hmacSha256 = Hmac(sha256, key); // HMAC-SHA256
+  // //   var digest = hmacSha256.convert(bytes);
+  // //   print("HMAC signature in string is: $digest");
+  // //   return digest;
+  // // }
+  //
+  // dynamic onAuthorizer(
+  //     String channelName, String socketId, dynamic options) async {
+  //   log("onAuthorizer $channelName : SocketId $socketId");
+  //   const String token = "60056|XQmYLu4O6AHiqNiqxwDOMEIuMSHmLCZVMSlIs7Y2";
+  //   var authUrl = "https://icircles.app/broadcasting/auth";
+  //
+  //   HttpWithMiddleware http = HttpWithMiddleware.build(middlewares: [
+  //     HttpLogger(logLevel: LogLevel.BODY),
+  //   ]);
+  //   dynamic result;
+  //   try {
+  //     result = await http.post(
+  //       Uri.parse(authUrl),
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //         'Authorization': 'Bearer $token',
+  //       },
+  //       body: 'channel_name=$channelName',
+  //     );
+  //   } catch (e) {
+  //     log(e.toString());
+  //   }
+  //
+  //   var json = jsonDecode(result.body);
+  //   return json;
+  // }
 }
--- lib/app/modules/pusher_chat_app/views/pusher_chat_app_view.dart
+++ lib/app/modules/pusher_chat_app/views/pusher_chat_app_view.dart
@@ -10,95 +10,15 @@ class PusherChatAppView extends GetView<PusherChatAppController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          controller.isPusherConnected.value
-              ? 'Pusher Channels Example'
-              : controller.channelName.text,
-        ),
+        title: const Text('Pusher Channels Example'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Obx(() {
-          return ListView(
-            controller: controller.listViewController,
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            children: [
-              if (!controller.isPusherConnected.value)
-                Form(
-                    key: controller.channelFormKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: controller.channelName,
-                          validator: (String? value) {
-                            return (value != null && value.isEmpty)
-                                ? 'Please enter your channel name.'
-                                : null;
-                          },
-                          decoration: const InputDecoration(
-                            labelText: 'Channel',
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: controller.onConnectPressed,
-                          child: const Text('Connect'),
-                        )
-                      ],
-                    ))
-              else
-                Form(
-                  key: controller.eventFormKey,
-                  child: Column(
-                    children: [
-                      ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: controller
-                              .pusher
-                              .channels[controller.channelName.text]
-                              ?.members
-                              .length,
-                          itemBuilder: (context, index) {
-                            final member = controller.pusher
-                                .channels[controller.channelName.text]?.members;
-                            return ListTile(
-                              title: Text(member.toString()),
-                              subtitle: Text(member.toString()),
-                            );
-                          }),
-                      TextFormField(
-                        controller: controller.eventName,
-                        validator: (String? value) {
-                          return (value != null && value.isEmpty)
-                              ? 'Please enter your event name.'
-                              : null;
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Event',
-                        ),
-                      ),
-                      TextFormField(
-                        controller: controller.data,
-                        decoration: const InputDecoration(
-                          labelText: 'Data',
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: controller.onTriggerEventPressed,
-                        child: const Text('Trigger Event'),
-                      ),
-                    ],
-                  ),
-                ),
-              const SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Text("test"),
-              ),
-            ],
-          );
-        }),
-      ),
+      body: Obx(() {
+        return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: controller.isPusherConnected.value
+                ? const Text("Connected")
+                : const Text("Connecting"));
+      }),
     );
   }
 }
